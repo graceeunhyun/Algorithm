e67151fdc7cfd65f9f91ce570213546f183b36c7
eunhyeon_kim <eunhyeon_kim@tmax.co.kr>
2019-08-01 11:53:31 +0900

    dsdsfs

---
+++ DFS, BFS/1260_DFS와BFS.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void dfs(int x)
+{
+	
+}
+
+int main(void)
+{
+	int n, m, v;
+	cin>>n>>m>>v;
+	int a[10][10];
+	//여기서 범위를 어떻게 해야하는지 고민.. 
+	for(int i =0; i<n; i++)
+	{
+		a[o] 
+	}
+	
+	//출력
+	//dfs 로 간 결과
+	//bfs 로 간 결과  
+}
