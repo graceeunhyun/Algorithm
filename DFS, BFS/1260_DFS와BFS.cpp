0fad5cc36aef7d4f492a51079ebad704d996f1a2
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2019-08-09 22:32:58 +0900

    Update 1260_DFS와BFS.cpp

--- DFS, BFS/1260_DFS와BFS.cpp
+++ DFS, BFS/1260_DFS와BFS.cpp
@@ -4,23 +4,38 @@
 
 using namespace std;
 
+//참고 사이트 :  https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98BOJ-1260%EB%B2%88-DFS%EC%99%80-BFSC
+#define MAX 1001;  
+
+int n, m, start;
+//array 를 보통 저렇게 MAX 로 선언한 
+int A[MAX][MAX]; //이렇게 최대값을 설정해서 MAX, MAX 로 설정해놓음
+int check[MAX];   //안에 방문했는지 안했는지 true, false 로 놔도 되고 아니면 그냥 0,1 로 해도된다 . 
+ 
 void dfs(int x)
 {
 	//dfs 
 	cout<<x<<'\n';
+	
 }
 
 int main(void)
 {
-	int n, m, v;
-	cin>>n>>m>>v;
+	int n, m, start;
+	cin>>n>>m>>start;
 	int a[10][10];
 	//여기서 범위를 어떻게 해야하는지 고민.. 
-	for(int i =0; i<n; i++)
+	for(int i =0; i<m; i++) //간선의 갯수만큼  
 	{
-		a[o] 
+		int num1, num2;
+		cin>>num1>>num2;
+		
+		//인접행렬로 설정 (다른 방법도 있을 텐데)  
+		A[num1][num2] == 1; 
+		A[num2][num1] == 1;
 	}
 	
+	dfs(start);	
 	//출력
 	//dfs 로 간 결과
 	//bfs 로 간 결과  
