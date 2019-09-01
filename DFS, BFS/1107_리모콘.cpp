fc9e03f36368c88d332f5e40bb43147ec4099588
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2019-09-01 22:07:49 +0900

    Update 1107_리모콘.cpp

--- DFS, BFS/1107_리모콘.cpp
+++ DFS, BFS/1107_리모콘.cpp
@@ -1,1 +1,21 @@
+#include <iostream> 
+using namespace std;
 
+int broken[10]; //고장난 번호의 수 
+int c, m;
+bool checkBroken(int c)
+{	
+	int len = 0; 
+	
+	
+	
+}
+int main(void)
+{
+	cin >> c;
+	while(true)
+	{
+		
+	}
+	
+}
