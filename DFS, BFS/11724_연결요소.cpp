e54c7ae88afad984c42a7213213b6d94dde4fde7
eunhyeon_kim <eunhyeon_kim@tmax.co.kr>
2019-09-30 20:18:46 +0900

    유기농 배추

---
+++ DFS, BFS/11724_연결요소.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 1000 + 1;
+int M, N;
+
+//vector<int> graph[MAX];
+int array[MAX][MAX];
+bool visited[MAX];
+
+void DFS(int current)
+{
+	currne	
+}
+
+int main(void)
+{
+	cin>> N >> M;
+//	while(cin>>)? // 그 입력이 있을 때까지 계속 오는 게 뭐였지?? 
+
+	//1. input 영역 
+	for (int i = 0; i< M ; i++) //간선의 수만큼 받을 수 있도록  
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+		
+		//이런식으로 array 에 채워넣어주기 	
+		array[a][b] = array[b][a];
+	}
+	
+	for(int i = 0 ; i< N ; i++)
+	{
+		
+	}
+	
+	
+}
+
--- DFS, BFS/1230_유기농배추.cpp
+++ DFS, BFS/1230_유기농배추.cpp
@@ -7,7 +7,7 @@ int M,N,K;
 //배추 땅 array 
 int arr[50][50]={0};
 //방문했는지 안했는지 check array  
-int visited[50][50]={0};
+int visited[50][50]={0,};
 // 배추가 잇는 위치를 모두 연결한 경우를 하나의 구역이라고 생각하고 이 구역이 몇개있는지 구하는 문제
 // DFS 를 통해 배추가 놓인 지역을 방문해서, 왼쪽과 오른쪽 구역을 탐색.. 
 
@@ -21,7 +21,7 @@ void dfs(int y, int x)
 		
 		//구역을 탐색해야한다. 
 		if(curY <0 || curY >= N || curX <=0 || curX >= M)
-			continue;    //이부분이 잘 이해가 안된다... 왜 continue 인지 
+			continue;    //continue: 계속해서 탐색.  
 		
 		if(arr[curY][curX] && ! visited[curX][curY])
 		{
@@ -36,4 +36,29 @@ int main(void)
 	int T; //흰지렁이 마리수 
 	//같은 구역인 부분을 정하기.  
 	//이제 입출력으로 읽어서 해당 부분 확인하자. 	
+	for(int j = 0; j<T ; j++) //테스트케이스  
+	{
+		scanf("%d %d %d", &M, &N, &K);
+		for(int i = 0 ; i<K ; i++)
+		{
+			int x, y = 0;
+			scanf("%d %d", &x, &y);
+			arr[y][x] = 1;
+		}	
+		
+		//계산 로직  
+		int count = 0; //지렁이수  
+		for(int i = 0; i<M ; i++)
+		{
+			
+			for(int j = 0; j<N; j++)
+			{
+				if(arr[i][j] == 1 )
+					count++;
+					dfs(j, i);
+			}
+		}
+	}
+	
+	 
 }
